7da9688d2d3ddd3e2677e7ad4548f45cf746e579
Ravi <ravi.kiran8844@gmail.com>
2023-03-16 22:58:42 +0530

    updated sections

---
+++ assets/faq-section.js
@@ -0,0 +1,30 @@
+  $(document).ready(function() {
+    var accordionParentClass = '.eachFaq';
+    var accordionQuestionClass = '.eachFaq .title';
+    var accordionContentClass = '.accordion-content';
+    $(accordionParentClass).each(function() {
+      $(this).addClass('close');
+      $(this).find(accordionContentClass).hide();
+    });
+
+    $(accordionQuestionClass).click(function() {
+      var faqClass = $(this).closest(accordionParentClass).attr('class');
+      console.log(faqClass);
+
+      if (faqClass.indexOf('close') != -1) { // WHEN CLOSED
+        $(accordionParentClass).find(accordionContentClass).slideUp('slow'); // CLOSE ALL
+        $(accordionParentClass).addClass('close').removeClass('open'); // set all faq as closed
+
+        $(this).closest(accordionParentClass).removeClass('close');
+        $(this).closest(accordionParentClass).addClass('open');
+        $(this).closest(accordionParentClass).find(accordionContentClass).slideDown('slow');
+
+      } else {
+        $(this).closest(accordionParentClass).addClass('close');
+        $(this).closest(accordionParentClass).removeClass('open');
+        $(this).closest(accordionParentClass).find(accordionContentClass).slideUp('slow');
+      }
+
+    });
+
+  });
